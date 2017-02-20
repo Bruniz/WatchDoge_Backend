0bdc9dafc12bcaed8018dc9a6bdf07e88f765dbd
Mathias Brunström <mathiasbrunstrom@gmail.com>
2017-02-20 17:55:00 +0200

    Small fix for favicon not showing up and regex .../*/* now works
    Regarding the regex, seems like it was just in the wrong order in
    urls.py
    Fixes #1

--- watchdoge/urls.py
+++ watchdoge/urls.py
@@ -7,8 +7,9 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', 'watchdogesite.views.home', name='home'),
     url(r'^about/', 'watchdogesite.views.about', name='about'),
+    url(r'^reports/add', 'watchdogesite.views.add_report', name='add_report'),
     url(r'^reports/', 'watchdogesite.views.reports', name='reports'),
-    url(r'^add_report', 'watchdogesite.views.add_report', name='add_report'),
+
 
 #    url(r'^admin/', include(admin.site.urls)),
 ]
