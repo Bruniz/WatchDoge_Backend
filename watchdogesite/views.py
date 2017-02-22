6f7a3c97c810389e21719417368097cad510900f
Mathias Brunström <mathiasbrunstrom@gmail.com>
2017-02-22 16:26:26 +0200

    Removed card token from add_report
    Can now send POST request to baseurl/reports/add

--- watchdogesite/views.py
+++ watchdogesite/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 from google.appengine.ext import db
 from watchdogesite.models import Report
-
+from django.views.decorators.csrf import csrf_exempt
 import datetime
 import logging
 
@@ -24,6 +24,7 @@ def reports(request):
     return render(request, "reports.html", {'reports': reports})
 
 
+@csrf_exempt
 def add_report(request):
     if request.method == 'POST':
         items = request.POST
