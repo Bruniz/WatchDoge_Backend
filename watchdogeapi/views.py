6e4c3ea2361da96a675b1e1f2bf7961a6edb58e3
Patrik <hillner.patrik@gmail.com>
2017-02-25 14:38:26 +0200

    Add checks if request is valid, add response whether the creation of report succeeded (contains report id) or not (returns server error)
    Checks done are:
    - request contains all parameters necessary
    - request check is valid

--- watchdogeapi/views.py
+++ watchdogeapi/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.http import HttpResponseNotAllowed
-import json
+from django.http import HttpResponseServerError
+from django.http import HttpResponseForbidden
 from watchdogesite.models import Report
 from django.views.decorators.csrf import csrf_exempt
 import datetime
@@ -8,19 +9,31 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+CHECK = hash('1337badwolf')
 
 @csrf_exempt
 def add(request):
 
     # Add new report
     if request.method == 'POST':
-        items = request.POST
-        r = Report(type=items['type'], description=items['desc'], title=items['title'],
-                   pets=items['pets'], entry=items['entry'])
-        r.date = datetime.datetime.now().date()
-        r.put()
-        return HttpResponse(r.key(),
-                            content_type='text/plain')
+        params = request.content_params
+        if (params.has_key('check') and params.has_key('type') and
+            params.has_key('desc') and params.has_key('title') and
+            params.has_key('pets') and params.has_key('entry')):
+            if request.POST['check'] is not CHECK:
+                return HttpResponseForbidden
+            try:
+                items = request.POST
+                r = Report(type=items['type'], description=items['desc'], title=items['title'],
+                           pets=items['pets'], entry=items['entry'])
+                r.date = datetime.datetime.now().date()
+                r.put()
+            except Exception, e:
+                return HttpResponseServerError('Failed to receive report')
+            return HttpResponse(r.key(),
+                                content_type='text/plain')
+        else:
+            return HttpResponseForbidden
 
     else:
         return HttpResponseNotAllowed('POST')
